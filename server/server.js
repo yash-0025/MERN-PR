87556c1d7650beed054fda55d92b96399f6c0d93
Yash Patel <yashpatel200403@gmail.com>
2021-12-06 12:08:22 +0530

    Setup basic Express js server.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("HEllo from server");
+console.log("HEllo from server")
+
+//*      [ Importing Express js ]
+
+const express = require("express")
+
+//&      { Initialising an app with express }
+const app = express()
+
+//^      Configure a Port
+const port = 5000
+
+//?      Listen to a port
+app.listen(port, () => {
+  console.log(`Server started on Port ${port}.`)
+})
