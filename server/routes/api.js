bdb16c6cb11a003632dd65a51408d737740d658f
Yash Patel <yashpatel200403@gmail.com>
2021-12-06 21:27:51 +0530

    Created skeleton structure for auth endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const app = express.Router();
 
+// Import the auths
+const auth = require("./api/auth.js")
+
 // Creating a get request handler for /.
 app.get("/", (req, res) => {
     res.json("Welcome to cC API.")
 })
 
+
+app.use("/auth", auth);
 module.exports = app;
---
+++ server/routes/api/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express.Router();
+
+// Creating a get request handler for /.
+
+
+module.exports = app;
