1857065e90b5679d69eb45e4b21b312a7a4acfd3
Yash Patel <yashpatel200403@gmail.com>
2021-12-06 12:19:56 +0530

    Minor changes in server file

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ const app = express()
 //^      Configure a Port
 const port = 5000
 
+
+// Creating a get server
+app.get("/", (req, res)=> {
+    res.json("Welcome to cC.")
+})
+
 //?      Listen to a port
 app.listen(port, () => {
   console.log(`Server started on Port ${port}.`)
